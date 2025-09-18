5e66d4e582e17bea6531dc5ba417c700b320e6ee
KalyanNagaSai-V <kalyan7716@gmail.com>
2025-09-18 11:35:38 +0530

    Add files via upload

---
+++ number_systems/excess3_binary.v
@@ -0,0 +1,5 @@
+module excess3_binary(input [3:0]a,output reg[3:0]y);
+  always@(*)begin
+    y=a-4'b0011;
+  end
+endmodule 
---
+++ number_systems/excess_binarytb.v
@@ -0,0 +1,14 @@
+module excess_binarytb();
+  reg [3:0]a;
+  wire [3:0]y;
+  
+ excess3_g dut(a,y);
+  integer i;
+  initial begin
+    $monitor("time:%0t a=%b,y=%b",$time,a,y);
+    for(i=0;i<=9;i=i+1)begin
+      a=i;
+    #10;
+    end
+  end
+endmodule
